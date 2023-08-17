54a7f81a00e175c0060cb4aab206dac0b2ee84c8
Bornali2019 <bornaliroychoudhury@gmail.com>
2023-08-17 15:55:38 +0530

    1st commit

---
+++ src/test/java/SS/GITSCM/ABTest.java
@@ -0,0 +1,11 @@
+package SS.GITSCM;
+
+import org.testng.annotations.Test;
+
+public class ABTest {
+	@Test
+	public void create() {
+		System.out.println("created");
+	}
+
+}
---
+++ src/test/java/SS/GITSCM/BBTest.java
@@ -0,0 +1,11 @@
+package SS.GITSCM;
+
+import org.testng.annotations.Test;
+
+public class BBTest {
+	@Test
+	public void delete() {
+		System.out.println("deleted");
+	}
+
+}
